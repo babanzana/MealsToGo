5743bf469af461adaa16de8f257f5c0e59798135
babanzana <babanzana610@gmail.com>
2023-02-22 18:11:05 +0700

    good

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -4,6 +4,7 @@ import styled from "styled-components/native";
 export const SafeArea = styled(SafeAreaView)`
   flex: 1;
   ${StatusBar.currentHeight};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 // && `margin-top: ${StatusBar.currentHeight}px`
